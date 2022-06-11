204c5485702b4e7de1698fd62e52464f598941e5
AshuAnand18 <106897228+AshuAnand18@users.noreply.github.com>
2022-06-11 22:11:23 +0530

    Create _app.js

---
+++ Pages/_app.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/header.css';
+import '../styles/Home.module.css'
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps }
+    />
+}
+
+export default MyApp
