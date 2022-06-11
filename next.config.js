0c2e448267207331b6000f8cfc61b50b4d5b95c6
AshuAnand18 <106897228+AshuAnand18@users.noreply.github.com>
2022-06-11 22:13:35 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ['www.freeiconspng.com', 'img.freepik.com', 'encrypted-tbn0.gstatic.com', 'www.hansairways.com', 'airwaysaviation.com', 'livefromalounge.com'],
+    },
+}
